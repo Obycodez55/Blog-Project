201db8b3f8803d4c74c31907d21cc25a3d6fc043
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2023-10-28 14:35:54 +0100

    Completed first post function

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var post = "";
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
-})
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  res.render("index.ejs");
 });
 
+app.post("/home", (req, res) => {
+  const header = req.body.post_heading;
+  const content = req.body.post_content;
+
+  post =
+    post + "<div><h1>" + header + "</h1>" + "<p>" + content + "</p> </div>";
+  const postData = {
+    post: post
+  };
+  res.render("index.ejs", postData);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
